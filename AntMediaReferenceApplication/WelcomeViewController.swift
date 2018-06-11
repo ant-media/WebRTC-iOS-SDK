8f9be6a61aa52f7561c5249ea3346f5a0e86a28a
Ogulcan <junkchorn@gmail.com>
2018-06-11 12:40:58 +0300

    Updated view controller

---
+++ AntMediaReferenceApplication/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AntMediaReferenceApplication
+//
+//  Created by Oğulcan on 11.06.2018.
+//  Copyright © 2018 AntMedia. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var logoTopAnchor: NSLayoutConstraint!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
