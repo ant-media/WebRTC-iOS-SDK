9c6017a90a29eb919db4841a0c51a730f673c20c
Ogulcan <junkchorn@gmail.com>
2018-06-11 13:31:33 +0300

    Updated welcome view controller ui

--- AntMediaReferenceApplication/WelcomeViewController.swift
+++ AntMediaReferenceApplication/WelcomeViewController.swift
@@ -12,8 +12,42 @@ class WelcomeViewController: UIViewController {
     
     @IBOutlet weak var logo: UIImageView!
     @IBOutlet weak var logoTopAnchor: NSLayoutConstraint!
+    @IBOutlet weak var actionContainer: UIView! {
+        didSet {
+            self.actionContainer.alpha = 0
+        }
+    }
+    @IBOutlet weak var roomField: UITextField!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var modeSelection: UISegmentedControl!
+    @IBOutlet weak var connectButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.5, delay: 1.0, options: .curveEaseOut, animations: {
+            self.logoTopAnchor.constant = 40
+            self.view.layoutIfNeeded()
+        }, completion: { (completed) in
+            UIView.animate(withDuration: 0.5, animations: {
+                self.actionContainer.alpha = 1
+                self.view.layoutIfNeeded()
+            })
+        })
+    }
+    
+    @IBAction func refreshTapped(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func connectButton(_ sender: UIButton ) {
+        
+    }
+    
+    @IBAction func serverTapped(_ sender: UIButton) {
+        
     }
 }
