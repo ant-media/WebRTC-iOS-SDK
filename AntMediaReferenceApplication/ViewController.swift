5bd426949233ea172b0ce3617993c372f94c15f9
Ogulcan <junkchorn@gmail.com>
2018-06-11 11:32:02 +0300

    Initial commit

---
+++ AntMediaReferenceApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AntMediaReferenceApplication
+//
+//  Created by Oğulcan on 11.06.2018.
+//  Copyright © 2018 AntMedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
