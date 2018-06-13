2cd756ec0a934dce742c798b2b55ace569e4cc08
Ogulcan <junkchorn@gmail.com>
2018-06-13 15:34:23 +0300

    Added AntMediaSDK

---
+++ AntMediaSDK.framework/Headers/AntMediaSDK.h
@@ -0,0 +1,16 @@
+//
+//  AntMediaSDK.h
+//  AntMediaSDK
+//
+//  Created by Oğulcan on 18/05/2018.
+//  Copyright © 2018 AntMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AntMediaSDK.
+FOUNDATION_EXPORT double AntMediaSDKVersionNumber;
+
+//! Project version string for AntMediaSDK.
+FOUNDATION_EXPORT const unsigned char AntMediaSDKVersionString[];
+
