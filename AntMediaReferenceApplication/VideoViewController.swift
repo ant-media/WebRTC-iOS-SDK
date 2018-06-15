5d548eb4e79f1d20726a0387e3bd1d87916fafeb
Ogulcan <junkchorn@gmail.com>
2018-06-15 22:43:15 +0300

    Added video assets w/ animation

--- AntMediaReferenceApplication/VideoViewController.swift
+++ AntMediaReferenceApplication/VideoViewController.swift
@@ -14,28 +14,60 @@ class VideoViewController: UIViewController {
     
     @IBOutlet weak var localVideoView: RTCEAGLVideoView!
     @IBOutlet weak var remoteVideoView: RTCEAGLVideoView!
+    @IBOutlet weak var containerView: UIView!
     
     // Auto Layout Constraints used for animations
     @IBOutlet weak var remoteViewTopConstraint: NSLayoutConstraint?
     @IBOutlet weak var remoteViewRightConstraint: NSLayoutConstraint?
     @IBOutlet weak var remoteViewLeftConstraint: NSLayoutConstraint?
     @IBOutlet weak var remoteViewBottomConstraint: NSLayoutConstraint?
+    @IBOutlet weak var containerLeftConstraint: NSLayoutConstraint?
     
     var client: AntMediaClient! {
         didSet {
-            self.client.setDebug(true)
+            self.client.setDebug(false)
             self.client.delegate = self
         }
     }
+    var tapGesture: UITapGestureRecognizer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.client.setVideoViews(local: localVideoView, remote: remoteVideoView)
         self.client.start()
+        self.setGesture()
+    }
+    
+    @IBAction func audioTapped(_ sender: UIButton!) {
+        
+    }
+    
+    @IBAction func videoTapped(_ sender: UIButton!) {
+        
     }
     
     @IBAction func closeTapped(_ sender: UIButton!) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func setGesture() {
+        self.tapGesture = UITapGestureRecognizer(target: self, action: #selector(VideoViewController.toggleContainer))
+        self.tapGesture.numberOfTapsRequired = 1
+        self.remoteVideoView.addGestureRecognizer(tapGesture)
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func toggleContainer() {
+        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            if (self.containerLeftConstraint!.constant <= -45.0) {
+                self.containerLeftConstraint!.constant = 15.0
+                self.containerView.alpha = 1.0
+            } else {
+                self.containerLeftConstraint!.constant = -45.0
+                self.containerView.alpha = 0.0
+            }
+            self.view.layoutIfNeeded()
+        })
     }
 }
 
