7d4cf1f016986d5f70d13eabd0bdec2a6624c8a8
Ogulcan <junkchorn@gmail.com>
2018-06-15 17:55:03 +0300

    Started client

--- AntMediaReferenceApplication/VideoViewController.swift
+++ AntMediaReferenceApplication/VideoViewController.swift
@@ -15,6 +15,12 @@ class VideoViewController: UIViewController {
     @IBOutlet weak var localVideoView: RTCEAGLVideoView!
     @IBOutlet weak var remoteVideoView: RTCEAGLVideoView!
     
+    // Auto Layout Constraints used for animations
+    @IBOutlet weak var remoteViewTopConstraint: NSLayoutConstraint?
+    @IBOutlet weak var remoteViewRightConstraint: NSLayoutConstraint?
+    @IBOutlet weak var remoteViewLeftConstraint: NSLayoutConstraint?
+    @IBOutlet weak var remoteViewBottomConstraint: NSLayoutConstraint?
+    
     var client: AntMediaClient! {
         didSet {
             self.client.delegate = self
@@ -28,7 +34,21 @@ class VideoViewController: UIViewController {
     }
     
     @IBAction func closeTapped(_ sender: UIButton!) {
-        self.dismiss(animated: true, completion: nil)
+        //self.dismiss(animated: true, completion: nil)
+        UIView.animate(withDuration: 0.4, animations: { () -> Void in
+            let containerWidth: CGFloat = self.view.frame.size.width
+            let containerHeight: CGFloat = self.view.frame.size.height
+            let defaultAspectRatio: CGSize = CGSize(width: 4, height: 3)
+
+            let aspectRatio: CGSize = defaultAspectRatio
+            let videoRect: CGRect = self.view.bounds
+            let videoFrame: CGRect = AVMakeRect(aspectRatio: aspectRatio, insideRect: videoRect)
+            
+            self.remoteViewTopConstraint!.constant = (containerHeight / 2.0 - videoFrame.size.height / 2.0)
+            self.remoteViewBottomConstraint!.constant = (containerHeight / 2.0 - videoFrame.size.height / 2.0) * -1
+            self.remoteViewLeftConstraint!.constant = (containerWidth / 2.0 - videoFrame.size.width / 2.0)
+            self.remoteViewRightConstraint!.constant = (containerWidth / 2.0 - videoFrame.size.width / 2.0)
+        })
     }
 }
 
@@ -41,5 +61,4 @@ extension VideoViewController: AntMediaClientDelegate {
     func clientDidDisconnect(_ message: String) {
         print("Disconnected")
     }
-    
 }
