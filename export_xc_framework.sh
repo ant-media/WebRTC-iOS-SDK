d31a61ba6a3d88c03452a6045522277a592eeeaf
mekya <ahmetmermerkaya@gmail.com>
2021-12-05 22:27:54 +0300

    Add prebuilt WebRTCiOSSDK.xcframework

--- export_xc_framework.sh
+++ export_xc_framework.sh
@@ -8,19 +8,18 @@ rm -rf $SIMULATOR_DIR
 rm -rf $OS_DIR
 
 Xcodebuild -workspace AntMediaReferenceApplication.xcworkspace -scheme WebRTCiOSSDK clean
-Xcodebuild -workspace AntMediaReferenceApplication.xcworkspace -scheme WebRTCiOSSDK -sdk iphonesimulator ARCHS=x64 -configuration Release install CONFIGURATION_BUILD_DIR=${SIMULATOR_DIR}
+Xcodebuild -workspace AntMediaReferenceApplication.xcworkspace -scheme WebRTCiOSSDK -sdk iphonesimulator ARCHS=x86_64 -configuration Release install CONFIGURATION_BUILD_DIR=${SIMULATOR_DIR}
 
 Xcodebuild -workspace AntMediaReferenceApplication.xcworkspace -scheme WebRTCiOSSDK -sdk iphoneos ARCHS=arm64 -configuration Release install CONFIGURATION_BUILD_DIR=$OS_DIR
 
 
 rm -rf $UNIVERSAL_DIR
 mkdir $UNIVERSAL_DIR
-cp -LR $OS_DIR/.  $UNIVERSAL_DIR
 
 Xcodebuild -create-xcframework -framework $OS_DIR/WebRTCiOSSDK.framework -framework $SIMULATOR_DIR/WebRTCiOSSDK.framework -output $UNIVERSAL_DIR/WebRTCiOSSDK.xcframework
 
 
-rm -rf $SIMULATOR_DIR
-rm -rf $OS_DIR
+#rm -rf $SIMULATOR_DIR
+#rm -rf $OS_DIR
 
 echo "If everything is ok. Your XC Framework is $UNIVERSAL_DIR/WebRTCiOSSDK.xcframework"
