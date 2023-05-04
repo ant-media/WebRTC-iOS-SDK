ff5a8858896725610b5e71275104d5c78800a5ae
mekya <ahmetmermerkaya@gmail.com>
2023-05-04 17:04:07 +0300

    Update Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+
+let package = Package(
+    name: "WebRTCiOSSDK",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "WebRTCiOSSDK",
+            targets: ["WebRTCiOSSDK"]
+        ),
+    ],
+    dependencies: [
+         .Package(url: "https://github.com/daltoniam/Starscream.git", majorVersion: 4)
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTCiOSSDK",
+            url: "WebRTCiOSSDK.xcframework",
+        ),
+    ]
+)
