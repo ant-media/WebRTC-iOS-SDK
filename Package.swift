b5cb9979ae7e7d56e8664014a932bbd55f614d1a
mekya <ahmetmermerkaya@gmail.com>
2023-05-05 14:55:43 +0300

    Update Package.swift and Embed frameworks to WebRTCiOS.xcframework

--- Package.swift
+++ Package.swift
@@ -9,22 +9,13 @@ let package = Package(
     products: [
         .library(
             name: "WebRTCiOSSDK",
-            targets: ["WebRTCiOSSDK", "WebRTC"]
+            targets: ["WebRTCiOSSDK"]
         ),
-    ],
-    dependencies: [
-         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.0"),
-   //      .package(url: "https://github.com/ant-media/WebRTC-iOS.git", .branch("main")),
-         
     ],
     targets: [
         .binaryTarget(
             name: "WebRTCiOSSDK",
             path: "WebRTCiOSSDK.xcframework"
-        ),
-        .binaryTarget(
-            name:"WebRTC",
-            path:"WebRTC.xcframework"
         )
     ]
 )
