bd0ce1bdc4d1ed5ef6c8d3f6673143a9efe6e681
mekya <ahmetmermerkaya@gmail.com>
2023-05-06 12:55:59 +0300

    Add WebRTC framework to targets

--- Package.swift
+++ Package.swift
@@ -9,12 +9,17 @@ let package = Package(
     products: [
         .library(
             name: "WebRTCiOSSDK",
-            targets: ["WebRTCiOSSDK"]
+            targets: ["WebRTCiOSSDK", "WebRTC"]
         ),
+    ],
+    dependencies: [
+            .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.4")
+
     ],
     targets: [
         .binaryTarget(
             name: "WebRTCiOSSDK",
+            //dependencies: ["Starscream"],
             path: "WebRTCiOSSDK.xcframework"
         ),
         .binaryTarget(
