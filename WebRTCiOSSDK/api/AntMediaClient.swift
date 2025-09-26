5e1cccbc98967fd76d24ea051cfc8d3209198066
Sieu To <121605093+sieutotu@users.noreply.github.com>
2025-09-26 14:23:11 +0700

    Update AntMediaClient.swift
    Update for IOS 26

--- WebRTCiOSSDK/api/AntMediaClient.swift
+++ WebRTCiOSSDK/api/AntMediaClient.swift
@@ -66,7 +66,7 @@ open class AntMediaClient: NSObject, AntMediaClientProtocol {
     private var p2pStreamId: String?
     private var publishToken: String?
     private var playToken: String?
-    private var webSocket: WebSocket?
+    private var webSocket: Starscream.WebSocket?
     // keep it for backward compatibility
     private var mode: AntMediaClientMode!
     var streamsInTheRoom: [String] = []
